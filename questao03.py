08a78b4d0b1e29ea92bec5af1fea8e7af431270e
athena272 <guilhermera272@gmail.com>
2024-03-08 13:34:23 -0300

    Create questao03.py

---
+++ questao03.py
@@ -0,0 +1,35 @@
+# a) 1, 3, 5, 7, ___
+
+# Lógica: Números ímpares crescentes em ordem ascendente.
+
+# Próximo elemento: 9
+################################
+# b) 2, 4, 8, 16, 32, 64, ____
+
+# Lógica: Potências de 2.
+
+# Próximo elemento: 128
+################################
+# c) 0, 1, 4, 9, 16, 25, 36, ____
+
+# Lógica: Quadrados dos números naturais.
+
+# Próximo elemento: 49
+################################
+# d) 4, 16, 36, 64, ____
+
+# Lógica: Quadrados dos números pares.
+
+# Próximo elemento: 100
+################################
+# e) 1, 1, 2, 3, 5, 8, ____
+
+# Lógica: Sequência de Fibonacci, onde cada número é a soma dos dois anteriores.
+
+# Próximo elemento: 13
+################################
+# f) 2, 10, 12, 16, 17, 18, 19, ____
+
+# Lógica: A resposta ‘certa’ para esse desafio seria 200, pois o ‘padrão’ esperado envolve os números Naturais que começam com a letra D.
+
+# Próximo elemento: 200
